52d521db452c7fa138e44949acad09e2947f2c47
Jason Chan <jasonchan07@berkeley.edu>
2020-01-11 17:55:20 -0800

    added legend

--- winter_break.R
+++ winter_break.R
@@ -16,7 +16,7 @@ airports_original <- data.table::fread('airports.csv', data.table = FALSE)
 routes_original <- data.table::fread('routes.csv', data.table = FALSE)
 
 #########################
-### CLEAN ROUTES DATA ###
+### CLEAN ROUTES DATA ###g
 #########################
 
 routes <- routes_original %>% 
@@ -64,11 +64,14 @@ generateFlights <- function(source, dest) {
     # obtain source city schema
     source_schema <- flights %>% 
                         filter(source_iata == source) %>% 
-                        dplyr::select(route, source_iata, source_long, source_lat)
+                        dplyr::select(source_city, source_iata, source_long, source_lat)
     dest_schema <- flights %>% 
                         filter(dest_iata == dest) %>% 
-                        dplyr::select(dest_iata, dest_long, dest_lat)
-    final_schema <- cbind(source_schema[1, ], dest_schema[1, ])
+                        dplyr::select(dest_city, dest_iata, dest_long, dest_lat)
+    final_schema <- cbind(source_schema[1, ], dest_schema[1, ]) %>% 
+                        mutate(route = paste(source_city, dest_city, sep = '-')) %>% 
+                        dplyr::select(route, everything(), -source_city, -dest_city)
+    
     return(final_schema)
 }
 
@@ -84,10 +87,10 @@ trip_flights <- flights %>%
     filter(source_iata == 'SFO' & dest_iata == 'DCA' | # shohini to DC
                source_iata == 'SFO' & dest_iata == 'DEN' | # jason, kiran to london
                source_iata == 'DEN' & dest_iata == 'LHR' | 
-               source_iata == 'LAX' & dest_iata == 'LHR' | # prabha to london
+               source_iata == 'LAX' & dest_iata == 'DCA' | # prabha to DC
+               source_iata == 'IAD' & dest_iata == 'LHR' | # prabha to London
                source_iata == 'SFO' & dest_iata == 'LHR' | # jeffrey, mihir to london
                source_iata == 'LHR' & dest_iata == 'OPO' | # everyone to porto, technically stansted
-               source_iata == 'OPO' & dest_iata == 'LIS' | # everyone to lisbon
                source_iata == 'LIS' & dest_iata == 'DXB'| # shohini to dubai
                source_iata == 'DXB' & dest_iata == 'DEL' |
                source_iata == 'LIS' & dest_iata == 'LHR' | # everyone to london, technically luton
@@ -97,26 +100,25 @@ trip_flights <- flights %>%
     dplyr::select(route, source_iata, source_long, source_lat, dest_iata, dest_long, dest_lat) %>% 
     rbind(manual_flights)
 
-
 # cities for plotting
 cities <- airports %>% 
             filter(iata %in% trip_flights$source_iata | iata %in% trip_flights$dest_iata)
             
                     
-                        
+                    
 #############
 ### PLOT ####
 #############  
 
 # get great circles for each flight
 gc_routes <- geosphere::gcIntermediate(trip_flights[c("source_long", "source_lat")],
-                            trip_flights[c("dest_long", "dest_lat")],
-                            n = 360, addStartEnd = TRUE, sp = TRUE, 
-                            breakAtDateLine = TRUE)
+                                       trip_flights[c("dest_long", "dest_lat")],
+                                       n = 360, addStartEnd = TRUE, sp = TRUE, 
+                                       breakAtDateLine = TRUE)
 
 gc_routes_spatial <- sp::SpatialLinesDataFrame(gc_routes, 
-                                   data.frame(route = trip_flights$route,
-                                              stringsAsFactors = FALSE))
+                                               data.frame(route = trip_flights$route,
+                                                          stringsAsFactors = FALSE))
 
 # load world shape file
 world <- raster::shapefile('world/ne_110m_admin_0_countries_lakes.shp')
@@ -124,6 +126,17 @@ world <- raster::shapefile('world/ne_110m_admin_0_countries_lakes.shp')
 # convert to dataframe
 world_df <- fortify(world)
 gc_routes_df <- fortify(gc_routes_spatial)
+
+groups <- c('Shohini', 'Mihir & Jeffrey', 'Kiran & Jason', 'Prabha', 'Prabha', 'Jason',
+           'Kiran, Jason, Mihir, Jeffrey, Prabha', 'Mihir & Jeffrey', 'Kiran, Jason, Mihir, Jeffrey, Prabha',
+           'Shohini', 'Kiran & Jason', 'Jason', 'Shohini', 'Shohini', 'Shohini', 'Kiran & Prabha')
+
+group_ids <- data.frame(id = unique(gc_routes_df$id), groups)
+
+# join gc_routes_df and name_ids
+gc_routes_final <- gc_routes_df %>%
+                    inner_join(group_ids, by = 'id') %>%
+                    mutate(groups = as.factor(groups))
 
 
 ggplot() +
@@ -137,8 +150,8 @@ ggplot() +
               size = 3, color = "grey20", alpha = 0.9, nudge_y = 2, 
               check_overlap = TRUE) +
     # routes
-    geom_path(data = gc_routes_df, 
-              aes(long, lat, group = group), alpha = 0.5, color = "#fa6900") +
+    geom_path(data = gc_routes_final, 
+              aes(long, lat, group = group, colour = groups), alpha = 0.5) +
     # aesthetics
     coord_proj("+proj=kav7") +
     scale_x_continuous(breaks = seq(-180, 180, 30)) +
@@ -147,4 +160,3 @@ ggplot() +
           axis.title = element_blank(),
           axis.text = element_blank(),
           axis.ticks = element_blank())
-                    
